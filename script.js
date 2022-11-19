f79efdbb4433dff4c43c88a6afd6a5a5354f8489
Caio Oliveira <caioinacio00@gmail.com>
2022-11-19 13:46:34 -0300

    fetch POST, PUT and DELETE

--- script.js
+++ script.js
@@ -23,19 +23,14 @@ $(document).ready(function(){
 		}
 	})
 })
-
+let clients
 fetch(`${URL}/clients`)
   .then(response => response.json())
+  .then(clients => clients = clients)
   .then(clients => clients?.map(client => {
 
     var tr =
             `<tr>
-                <td>
-                    <span class="custom-checkbox">
-                        <input type="checkbox" id="checkbox1" name="options[]" value="1">
-                        <label for="checkbox1"></label>
-                    </span>
-                </td>
                 <td id="${client.id}">${client.id}</td>
                 <td>${client.name}</td>
                 <td>${client.phone}</td>
@@ -43,13 +38,12 @@ fetch(`${URL}/clients`)
                 <td>${client.address}</td>
                 <td>${client.country}</td>
                 <td>
-                    <a href="#editEmployeeModal" onclick="loadForm(document.getElementById('${client.id}').innerHTML)" class="edit" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a>
-                    <a href="#deleteEmployeeModal" class="delete" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>
+                    <a href="#editEmployeeModal" onclick="loadFormEdit(document.getElementById('${client.id}').innerHTML)" class="edit" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a>
+                    <a href="#deleteEmployeeModal" onclick="loadFormDel(document.getElementById('${client.id}').innerHTML)" class="delete" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>
                 </td>
             </tr>`
 
     tbody.innerHTML += tr
-    //document.getElementById().onclick = function(){ alert("g") }
 }))
 
 var tbody = document.getElementById('tbody')
@@ -57,29 +51,27 @@ var tbody = document.getElementById('tbody')
 
 
 function addClient() {
-    data = {
-        name: document.getElementById('nameAdd').value,
-        phone: document.getElementById('phoneAdd').value,
-        email: document.getElementById('emailAdd').value,
-        address: document.getElementById('addressAdd').value,
-        country: document.getElementById('countryAdd').value
-    }
+  data = {
+    name: document.getElementById('nameAdd').value,
+    phone: document.getElementById('phoneAdd').value,
+    email: document.getElementById('emailAdd').value,
+    address: document.getElementById('addressAdd').value,
+    country: document.getElementById('countryAdd').value
+  }
 
-    fetch(`${URL}/clients`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data),
-      })
-        .then((response) => console.log(response.json()))
-    
+  fetch(`${URL}/clients`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json', },
+      body: JSON.stringify(data),
+    })
+  alert("Client addeded successfuly")
 }
 
-function loadForm(id) {
-    fetch(`${URL}/clients/${id}`)
+function loadFormEdit(id) {
+  fetch(`${URL}/clients/${id}`)
   .then(response => response.json())
   .then(client => {
+    document.getElementById('idEdit').innerHTML = client.id
     document.getElementById('nameEdit').value = client.name
     document.getElementById('phoneEdit').value = client.phone
     document.getElementById('emailEdit').value = client.email
@@ -89,5 +81,44 @@ function loadForm(id) {
 }
 
 function editClient() {
-    
+  const id = document.getElementById('idEdit').innerHTML
+  data = {
+    name: document.getElementById('nameEdit').value,
+    phone: document.getElementById('phoneEdit').value,
+    email: document.getElementById('emailEdit').value,
+    address: document.getElementById('addressEdit').value,
+    country: document.getElementById('countryEdit').value
+  }
+
+  fetch(`${URL}/clients/${id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data)
+  })
+  alert("Client edited successfuly")
 }
+
+function loadFormDel(id) {
+  fetch(`${URL}/clients/${id}`)
+  .then(response => response.json())
+  .then(client => {
+    document.getElementById('idDel').innerHTML = client.id
+    document.getElementById('nameDel').innerHTML = client.name
+  })
+}
+
+function deleteClient() {
+  const id = document.getElementById('idDel').innerHTML
+  fetch(`${URL}/clients/${id}`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    }
+  })
+  .then(response => console.log(response.json()))
+  alert("Client deleted successfuly")
+}
+
+
